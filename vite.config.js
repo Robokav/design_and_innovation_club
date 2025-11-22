bb3c964325abc01fefc5f5ecf588acc274f6045e
Robokav <kavinbavisi@gmail.com>
2025-11-23 01:28:32 +0530

    stexml

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
       base: '/', 
   plugins: [
     react(),
+     Sitemap({
+      hostname: 'https://design-and-innovation-club.vercel.app', // IMPORTANT: Use your actual Vercel domain
+      
+      // Define the static routes that don't come from data
+      routes: [
+        '/', 
+        '/projects', 
+        '/projects/active', 
+        '/projects/3d-models',
+        '/team/professor',
+        '/team/core',
+        '/team/junior',
+        '/contact'
+      ],}),
+      
      ViteImageOptimizer({
       /* config options */
       png: {
